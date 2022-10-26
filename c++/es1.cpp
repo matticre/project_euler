b4de7807376d3396dabb05fa51efda727e053679
catomaior <matticre@protonmail.com>
2022-10-26 14:23:38 +0200

    New file

---
+++ c++/es1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int divisors (int n){
+
+  int sum = 0;
+    
+    for (int i=1; i<n; i++){
+    if (i%3==0){
+      sum += i;
+    }
+
+    else if (i%5==0){
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+
+int main() {
+
+  cout << divisors(1000) << endl;
+
+}
